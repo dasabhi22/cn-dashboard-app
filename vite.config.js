26a2cf23bf5947e483e620b37d2d6547ccdf7806
Abhiraj Das <abhirajdas2207001@gmail.com>
2025-04-26 01:49:13 +0530

    cn-dashboard-app vite config chnges

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const repoName = '/cn-dashboard-app'
-
 export default defineConfig({
-  base: repoName,
+
+  base: '/cn-dashboard-app/',
   plugins: [
     react(),
     tailwindcss(),
